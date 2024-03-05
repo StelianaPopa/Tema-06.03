d4c7a62e5fe480c648800f1a0bec2957927f5b1c
Popa Steliana <stelianagurgulin43@gmail.com>
2024-03-05 11:09:23 +0200

    predare tema

---
+++ script.js
@@ -0,0 +1,23 @@
+// Ex 1) Sa se creeze o variabila reprezentand numele userului curent. Variabila sa fie de tip string, ca valoare poate fi orice
+let userId = "Popa Steliana";
+
+// Ex 2) Sa se creeze o variabila reprezentand statusul userului curent - daca este administrator sau nu. Variabila sa aiba numele esteAdmin si sa fie de tip boolean
+let esteAdmin = true;
+
+// Ex 3) Sa se creeze o variabila a de tip number si sa aiba valoarea 20, o variabila de tip number si sa aiba valoarea 7, sa se afiseze in consola suma, diferenta, inmultirea si impartirea lor.
+let a = 20;
+let b = 7;
+let sumaAB = a + b;
+let diferentaAB = a -b;
+let inmultireAB = a * b;
+let impartireAB = a / b;
+
+console.log(sumaAB)
+console.log(diferentaAB)
+console.log(inmultireAB)
+console.log(impartireAB)
+
+
+// Ex 4) Extra - sa se afiseze in consola lungimea string-ului de la exercitiul 1. De exemplu, daca nunele userului curent este "Cristian" sa se afiseze in consola
+let lungimeText = userId.length;
+console.log(lungimeText);
